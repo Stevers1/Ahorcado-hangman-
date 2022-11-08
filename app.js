7230a0d2ccc6b55f6bd384034f806c9529d9d5bc
Steve <uberkootra@gmail.com>
2022-11-08 12:10:02 -0600

    added js Dom

--- app.js
+++ app.js
@@ -1,71 +1,88 @@
-function game() {
-  let word = wordSelect();
-  let palabraSinLetras = [];
-  let userSeleccion
-  let counter = true;
-  let letrasUsadas =[];
-  let vidas = 5;
-  for( let i = 0 ; i < word.length; i++){
-    palabraSinLetras.push("_")
-  }
-  console.log(palabraSinLetras)
-  
-  do {
+let word = wordSelect();
+let palabraSinLetras = [];
+let userSeleccion;
 
-    userSeleccion = prompt(`Bienvenido al Ahorcado (Hangman) favor de ingresar letra por letra\n\nEsta es la Palabra: \n ${palabraSinLetras}\n Te quedan ${vidas} vidas\n\n Letras usadas: ${letrasUsadas}`)
+let letrasUsadas2 = [];
+let vidas = document.querySelector(".lives");
+vidas.innerText = 5;
+const letras = [
+  "q",
+  "w",
+  "e",
+  "r",
+  "t",
+  "y",
+  "u",
+  "i",
+  "o",
+  "p",
+  "a",
+  "s",
+  "d",
+  "f",
+  "g",
+  "h",
+  "j",
+  "k",
+  "l",
+  "z",
+  "x",
+  "c",
+  "v",
+  "b",
+  "n",
+  "m",
+];
+const keys = document.querySelector(".container-keys");
+const usedWords = document.querySelector(".used_words");
 
-    letrasUsadas.push(userSeleccion)
+const wordDisplay = document.querySelector(".word");
+
+for (let i = 0; i < word.length; i++) {
+  palabraSinLetras.push("_");
+}
 
 
-    if (word.includes(userSeleccion)){
+wordDisplay.innerText = palabraSinLetras;
+
+letras.forEach((letra, index) => {
+  const key = document.createElement("div");
+  key.setAttribute("class", `key-${letra}`);
+  key.setAttribute("id", `key`);
+  key.innerText = `${letra}`;
+  keys.append(key);
+});
+
+const keyNode = document.querySelectorAll("#key");
+
+keyNode.forEach((element) => {
+  element.addEventListener("click", () => {
+    userSeleccion = element.innerText;
+    letrasUsadas2.push(element.innerText);
+    console.log(letrasUsadas2);
+    usedWords.innerText = letrasUsadas2;
+    if (word.includes(userSeleccion)) {
       for (let i = 0; i < word.length; i++) {
-        
-        if(word[i] === userSeleccion) {
+        if (word[i] === userSeleccion) {
           palabraSinLetras[i] = userSeleccion;
-          console.log("correcto")
-          console.log(palabraSinLetras)
+          console.log("correcto");
+          console.log(palabraSinLetras);
         }
-        
+        wordDisplay.innerText = palabraSinLetras;
       }
-    } else vidas -= 1
+    } else vidas.innerText -= 1;
+  });
+});
 
-    if(vidas == 0){
-      alert(`Tus vidas se acabaron, perdiste`)
-      counter = false
-    }
+function wordSelect() {
+  let randomNum = Math.floor(Math.random() * (4 - 1) + 1);
+  console.log(randomNum);
 
-    console.log(word,palabraSinLetras.join(""))
-    console.log(word == palabraSinLetras.join(""))
-
-    if(word == palabraSinLetras.join("")){
-      alert (`Ganaste la palabra es ${word}`)
-      counter = false
-    }
-  }while( counter == true)
-}
-
-function randomNum (){ 
-
-  return Math.floor(Math.random()*(4 - 1)+1)
-}
-
-function wordSelect(){
-  let randomNum = Math.floor(Math.random()*(4 - 1)+1);
-  console.log(randomNum)
-
-  if(randomNum == 1){
-    return "calabaza"
-  }
-
-  else if (randomNum == 2){
-    return "koala"
-  }
-
-  else if(randomNum == 3){
-    return "coderhouse"
+  if (randomNum == 1) {
+    return "calabaza";
+  } else if (randomNum == 2) {
+    return "koala";
+  } else if (randomNum == 3) {
+    return "coderhouse";
   }
 }
-
-
-
-game();
